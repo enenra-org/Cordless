4f2b0da975bda9e97dfbd2b15df20358edc6c024
Jackson Abney <hickorysmokedbacon.yt@gmail.com>
2019-05-06 15:19:11 -0800

    Updated variable name and changed ready channel find method.

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
-//you can add the discord bot code here!
 const discord = require('discord.js');
-const config = require("./private_data/config.json");
+const privateConfig = require("./private_data/config.json");
 
 const client = new discord.Client();
 client.on('ready', () => {
     console.log('Tecton bot on and connected');
-    client.channels.find(x => x.name === 'bot-testing').send('Bot Operational');
+    client.channels.get('575022379756027904').send('Bot Operational');
 });
-client.login(config.token);
+client.login(privateConfig.token);
