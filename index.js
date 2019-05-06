74388849f6c3466bac26dc81ceb2af49ef56aba2
Hacker719 <neel.redkar@gmail.com>
2019-05-06 11:19:31 -0700

    basic connection

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 //you can add the discord bot code here!
+const discord = require('discord.js');
+const token = '***REMOVED***';
+
+const client = new discord.Client();
+client.on('ready',() => {
+    console.log('Tecton bot on and connected');
+    client.channels.find(x => x.name === 'bot-testing').send('Bot Operational');
+});
+client.login(token);
