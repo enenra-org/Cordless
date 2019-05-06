36a9d570b03a81f3e241e7f50a2f277636df7f52
Jackson Abney <hickorysmokedbacon.yt@gmail.com>
2019-05-06 15:04:23 -0800

    Removed token from being stored on the Git Hub.

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 //you can add the discord bot code here!
 const discord = require('discord.js');
-const token = '***REMOVED***';
+const config = require("./private_data/config.json");
 
 const client = new discord.Client();
 client.on('ready', () => {
     console.log('Tecton bot on and connected');
     client.channels.find(x => x.name === 'bot-testing').send('Bot Operational');
 });
-client.login(token);
+client.login(config.token);
